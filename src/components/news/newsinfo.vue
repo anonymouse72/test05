81a75484f839bf285fd8b0a073ad92861792021c
anonymouse72 <30422079+anonymouse72@users.noreply.github.com>
2017-08-20 09:36:27 +0800

    Update newsinfo.vue

--- src/components/news/newsinfo.vue
+++ src/components/news/newsinfo.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="tmpl">
-      <!--{{ this.$route.params.id }}-->
         <div class="title">
             <p class="ptitle">
                 {{article.title}}
@@ -11,10 +10,8 @@
                 分类：经济民生
             </p>
         </div>
-        <div class="content" v-html = "article.content">
-
-        </div>
-<!--3.0 评论组件的使用-->
+        <div class="content" v-html = "article.content"></div>
+    <!--3.0 评论组件的使用-->
         <div>
             <subcomment :artid="artid"></subcomment>
         </div>
@@ -66,11 +63,13 @@
             //负责根据传入的id值从 /api/getnew/:newid 去获取到真正的数据
             // 1.0  获取到url传入的id值
             let id  = this.$route.params.id;
+            
             //2.0 拼接url
             let url =common.apihost +'/api/getnew/' + id;
 
             //3.0 ajax请求获得数据
             this.$http.get(url).then(res=>{
+            
                 //获取图文资讯对象，但是message是一个数组，所以应该取0索引位置数据
                 this.article  =res.body.message[0];
 
